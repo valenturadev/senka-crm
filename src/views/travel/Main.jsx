6ee5a46ef8ba2f8005f90393af2334b97b86c95c
thesammet <thesammet@gmail.com>
2023-09-16 19:51:03 +0300

    addition: mutabakat forms for web controller

--- src/views/travel/Main.jsx
+++ src/views/travel/Main.jsx
@@ -89,7 +89,7 @@ function Main() {
                           scope="col"
                           className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
                         >
-                          İsim
+                          İsim - Mail
                         </th>
                         <th scope="col" className="relative px-6 py-3">
                           <span className="sr-only">Edit</span>
