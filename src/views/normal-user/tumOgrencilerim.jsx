4788e6a7895effc5b9a5d3f22910bdcf50d440b9
thesammet <thesammet@gmail.com>
2023-10-03 21:19:57 +0300

    refactoring: normal user - all students

--- src/views/normal-user/tumOgrencilerim.jsx
+++ src/views/normal-user/tumOgrencilerim.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link, useNavigate  } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function TumOgrencilerim() {
   const [students, setStudents] = useState([]);
@@ -11,7 +11,7 @@ export default function TumOgrencilerim() {
     // Bearer tokeni al
     let localUser = localStorage.getItem("user");
     let myUser = JSON.parse(localUser);
-    
+
     // Öğrenci verilerini çek
     axios({
       method: 'GET',
@@ -21,13 +21,13 @@ export default function TumOgrencilerim() {
         "Authorization": `Bearer ${myUser?.access}`
       }
     })
-    .then((response) => {
-      setStudents(response.data.data);
-    })
-    .catch((error) => {
-      setError('API çağrısı sırasında hata oluştu');
-      console.error('API çağrısı sırasında hata oluştu:', error);
-    });
+      .then((response) => {
+        setStudents(response.data.data);
+      })
+      .catch((error) => {
+        setError('API çağrısı sırasında hata oluştu');
+        console.error('API çağrısı sırasında hata oluştu:', error);
+      });
   }, []);
 
   function renderBoolean(value) {
@@ -63,13 +63,25 @@ export default function TumOgrencilerim() {
                 Öğrenci Soyadı
               </th>
               <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Öğrenci TC
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Öğrenci Doğum Tarihi
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Öğrenci Cinsiyet
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Öğrenci Sınıf
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Okul
               </th>
               <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Kampus
               </th>
               <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Telefon
+                Veli Telefon
               </th>
               <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Email
@@ -100,6 +112,10 @@ export default function TumOgrencilerim() {
                 <td className="px-6 py-4 whitespace-nowrap">{student.gezi_adi}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{student.ogrenci_adi}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{student.ogrenci_soyadi}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{student.ogrenci_tc}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{student.ogrenci_dogum_tarihi}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{student.ogrenci_cinsiyet_erkek == true ? "Erkek" : "Kız"}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{student.ogrenci_sinif}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{student.ogrenci_okul}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{student.ogrenci_kampus}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{student.ogrenci_phone}</td>
@@ -108,25 +124,25 @@ export default function TumOgrencilerim() {
                 <td className="px-6 py-4 whitespace-nowrap">{renderBoolean(student.is_bank_transfer)}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{renderBoolean(student.is_cash)}</td>
                 <td className="px-6 py-4 whitespace-nowrap">
-          {/* Link'i kullanarak öğrenci sayfasına yönlendir */}
-          <Link
-            to={`/sozlesme-onay/tel/${student.ogrenci_phone}/id/${student.id}`}
-            style={{ textDecoration: 'underline' }}
-          >
-            {renderBoolean(student.is_izin_formu)}
-          </Link>
-        </td>
+                  {/* Link'i kullanarak öğrenci sayfasına yönlendir */}
+                  <Link
+                    to={`/sozlesme-onay/tel/${student.ogrenci_phone}/id/${student.id}`}
+                    style={{ textDecoration: 'underline' }}
+                  >
+                    {renderBoolean(student.is_izin_formu)}
+                  </Link>
+                </td>
                 <td className="px-6 py-4 whitespace-nowrap">{renderBoolean(student.is_odeme_tamamlandi)}</td>
                 {/* Diğer sütunlar burada eklenir */}
                 <td className="px-6 py-4 whitespace-nowrap">
-          {/* İptal et butonunu ekleyin ve tıklama işlevini tanımlayın */}
-          <button
-            onClick={() => handleIptalEtClick(student.id)}
-            className="text-red-600 hover:text-red-800"
-          >
-            İptal Et
-          </button>
-        </td>
+                  {/* İptal et butonunu ekleyin ve tıklama işlevini tanımlayın */}
+                  <button
+                    onClick={() => handleIptalEtClick(student.id)}
+                    className="text-red-600 hover:text-red-800"
+                  >
+                    İptal Et
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
